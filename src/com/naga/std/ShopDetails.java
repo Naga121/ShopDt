a3f6353df376a1b7b271fd480faca1708277b544
thota <thota@DESKTOP-3RFTA5P>
2020-07-23 21:47:34 +0530

    Thard time

--- src/com/naga/std/ShopDetails.java
+++ src/com/naga/std/ShopDetails.java
@@ -3,5 +3,18 @@ package com.naga.std;
 public class ShopDetails {
 	private int sid;
 	private String sname;
+	ShopDetails(int sid,String sname){
+		this.sid=sid;
+		this.sname=sname;
+	}
+	public int getSid() {
+		return sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	void printShopdt() {
+		System.out.println( sid+","+sname);
+	}
 
 }
---
+++ src/com/naga/std/Test.java
@@ -0,0 +1,14 @@
+package com.naga.std;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ShopDetails sd=new ShopDetails(10,"Naga");
+		sd.getSid();
+		sd.getSname();
+		sd.printShopdt();
+		
+
+	}
+
+}
