d1f007bb43891431239b5b68fc5c6b6a59375863
thota <thota@DESKTOP-3RFTA5P>
2020-07-24 07:07:03 +0530

    Next Commit

--- src/com/naga/std/Test.java
+++ src/com/naga/std/Test.java
@@ -3,10 +3,11 @@ package com.naga.std;
 public class Test {
 
 	public static void main(String[] args) {
-		ShopDetails sd=new ShopDetails();
-		sd.getSid();
-		sd.getSname();
-		sd.printShopdt();
+		ShopDetails sd=new ShopDetails(10,"Naga");
+		ShopDetails sd1=new ShopDetails(20,"Raju");
+		System.out.println(sd.getSid()+","+sd.getSname());
+		System.out.println(sd1.getSid()+","+sd1.getSname());
+		
 		
 
 	}
